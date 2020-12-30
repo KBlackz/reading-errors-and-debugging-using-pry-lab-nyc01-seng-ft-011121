01f6f8bda1c68c6f48326863014fd3f601118a2c
Karris  Black <karris.b@gmail.com>
2020-12-30 23:50:30 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,6 +1,7 @@
 # don't forget to add: require 'pry'
-
+pry
 def get_user_input
+  binding.pry
   gets.chomp
 end
 
@@ -12,9 +13,9 @@ end
 
 def selection(num)
   if num = 1
-     "YUM YUM MUNCH MUNCH MUNCH"
+     puts "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
+     puts "HAM HAM HAM IN MY TUMMY"
   end
 end
 
